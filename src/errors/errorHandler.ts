a349a729f1f586f2ea75b1c598043bc0bcfb7fa5
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:18:42 +0500

    fix: memory leak muammosi hal qilindi

--- src/errors/errorHandler.ts
+++ src/errors/errorHandler.ts
@@ -1,6 +1,7 @@
 export class ErrorHandler extends Error{
     public message: string
     public status: number
+// kod formatlash va indentatsiya
 
 // code comments qo'shildi
 // API endpoint testlari qo'shildi
